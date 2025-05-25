7e4e2853a6c0583e0f5d720cb55c9a0c119f90e9
igi@igi-ThinkPad-X260 <igi-ThinkPad-X260@phpapps.cz>
2025-05-25 02:32:02 +0200

    lint automatization fix attempt

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,38 +1,16 @@
-// .eslintrc.js
-// ESLint configuration using Airbnb style guide and Prettier integration
-
 module.exports = {
-    root: true,
-    env: {
-      node: true,
-      jest: true,
-      es6: true
-    },
-    parserOptions: {
-      ecmaVersion: 2020,
-      sourceType: 'script'
-    },
-    extends: [
-      'airbnb-base',              // základní pravidla Airbnb pro JavaScript
-      'plugin:prettier/recommended' // propojení s Prettier pro formátování
-    ],
-    plugins: [
-      'import'                    // kontrola import/export syntaxe
-    ],
-    rules: {
-      // Povolit console.log pro jednoduché logování
-      'no-console': 'off',
-  
-      // Ukončení souboru novým řádkem
-      'eol-last': ['error', 'always'],
-  
-      // Povolit vulnerabilní require v testech a konfiguracích
-      'import/no-extraneous-dependencies': ['error', {
-        devDependencies: ['**/test/**/*.js', '**/*.spec.js', 'examples/**', 'docs/**']
-      }],
-  
-      // Mírnější pravidlo pro maximální délku řádku, Prettier se o to postará
-      'max-len': ['warn', { code: 100 }]
-    }
-  };
-  
+  root: true,
+  env: {
+    node: true,
+    es2021: true,
+    jest: true
+  },
+  extends: [
+    'eslint:recommended',      // jen built-in JS pravidla
+    'plugin:prettier/recommended'
+  ],
+  rules: {
+    'no-console': 'off',
+    'prettier/prettier': 'error',
+  }
+};
